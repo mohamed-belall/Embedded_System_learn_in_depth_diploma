7a4bfe30849913701d8893b7d8dfc7d3f4840996
mohamed-belall <mohamedbelal045@gmail.com>
2023-02-07 00:59:52 +0200

    solve ex_1 in unit 2 => assignment 1

---
+++ Unit_2_C_Programming/Assignment_1_C_Basics/ex_1.c
@@ -0,0 +1,21 @@
+/*
+==================================================================
+Name        : EX1: Write C Program to Print a Sentence
+Author      : Mohamed Belal
+Created on  : 7/2/2023
+Description : Unit 2 C-Programming => Assignment 1 C Basics => EX 1
+==================================================================
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\n\n============ Console-output ============ \n\n"); //for a good view in terminal
+
+    printf("C Programming");
+
+    printf("\n\n============ Console-output ============ \n\n"); //for a good view in terminal
+
+    return 0;
+}
