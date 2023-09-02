d7537a9c8929683a251d0b6aa50f110e3b7480c8
mohamed belal <mohamedbelal045@gmail.com>
2023-09-03 01:07:45 +0300

    add U7L3 gpio,lcd,keypad driver

---
+++ Unit_7_MCU_Essential_Peripherals/Lesson_3_GPIO_Part3_GPIO_Driver/LAB_2_lcd_keypad_gpio_driver_stm32/main.c
@@ -0,0 +1,122 @@
+
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : mohamed belal
+ * @Created on     : 31/8/2023
+ ******************************************************************************
+ */
+
+
+#include "keypad_Driver.h"
+#include "lcd_driver.h"
+
+uint8_t segments_hex[] = {
+		0x3F,  // 0
+		0x06,  // 1
+		0x5B,  // 2
+		0x4F,  // 3
+		0x66,  // 4
+		0x6D,  // 5
+		0x7D,  // 6
+		0x07,  // 7
+		0x7F,  // 8
+		0x6F   // 9
+};
+
+
+
+
+void GPIO_SevenSegment_Init(void)
+{
+
+	GPIO_PinConfig_t PinCfg;
+
+
+	PinCfg.GPIO_PinNumber = GPIO_PIN_3;
+	PinCfg.GPIO_MODE = GPIO_MODE_OUTPUT_PP;
+	PinCfg.GPIO_output_speed = GPIO_SPEED_10M;
+	MCAL_GPIO_Init(GPIOB, &PinCfg);
+
+
+	PinCfg.GPIO_PinNumber = GPIO_PIN_4;
+	MCAL_GPIO_Init(GPIOB, &PinCfg);
+
+
+	PinCfg.GPIO_PinNumber = GPIO_PIN_5;
+	MCAL_GPIO_Init(GPIOB, &PinCfg);
+
+	PinCfg.GPIO_PinNumber = GPIO_PIN_6;
+	MCAL_GPIO_Init(GPIOB, &PinCfg);
+
+	PinCfg.GPIO_PinNumber = GPIO_PIN_7;
+	MCAL_GPIO_Init(GPIOB, &PinCfg);
+
+	PinCfg.GPIO_PinNumber = GPIO_PIN_8;
+	MCAL_GPIO_Init(GPIOB, &PinCfg);
+
+	PinCfg.GPIO_PinNumber = GPIO_PIN_9;
+	MCAL_GPIO_Init(GPIOB, &PinCfg);
+
+}
+
+void Clock_Init(void)
+{
+
+	//ENABLE CLOCK GPIOA
+	//RCC_APB2ENR |= RCC_IOPAEN;
+	RCC_GPIOA_CLK_EN();
+	//ENABLE CLOCK GPIOB
+	//RCC_APB2ENR |= RCC_IOPBEN;
+	RCC_GPIOB_CLK_EN();
+	RCC_GPIOC_CLK_EN();
+
+}
+
+
+int main(void)
+{
+	volatile uint8_t key_pressed;
+	uint8_t i;
+
+	Clock_Init();
+	GPIO_SevenSegment_Init();
+	LCD_Init();
+
+	LCD_Send_String("hey there");
+	delay_ms(500);
+	LCD_Clear_Screen();
+
+	for(i=0;i<11;i++)
+	{
+		MCAL_GPIO_WritePort(GPIOB, segments_hex[i] << 3);
+		LCD_Display_Number(i);
+		delay_ms(500);
+	}
+	LCD_Clear_Screen();
+
+	keypad_Init();
+
+	LCD_Send_String("keypad is Ready");
+	delay_ms(500);
+	LCD_Clear_Screen();
+
+	while(1)
+	{
+		key_pressed = keypad_GetKey();
+		switch (key_pressed) {
+		case 'A':
+			break;
+		case '?':
+			LCD_Clear_Screen();
+			break;
+		default:
+			LCD_Send_Character(key_pressed);
+			break;
+		}
+
+
+	}
+}
+
+
